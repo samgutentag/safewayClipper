342fd99ebf4ccc65882d0b7d88daf5e933cebcc1
Sam Gutentag <sam@gutentag.co>
2018-11-02 20:25:04 -0700

    initial commit

---
+++ safewayClipper.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+''' A Tool for Clipping Coupons on Safeway.com!
+
+    You will need to install ChromeDriver, download link here:
+        https://sites.google.com/a/chromium.org/chromedriver/
+    Current Version is 2.43
+
+    Download Operating System SPecific Version!
+
+
+'''
+
+
+from selenium import webdriver
+
+
+__author__ = "Sam Gutentag"
+__copyright__ = "Copyright 2018, Sam Gutentag"
+__credits__ = ["Sam Gutentag"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Sam Gutentag"
+__email__ = "developer@samgutentag.com"
+__status__ = "Prototype"
+
+
+
+def main():
+
+
+    # open a browser window with default/Firefox
+    # driver = webdriver.Firefox()
+    # driver.get("www.samgutentag.com")
+
+    chromedriver = './chromedriver'
+    driver = webdriver.Chrome(chromedriver)
+    driver.get("http://www.safeway.com")
+
+    # login data
+
+
+
+
+
+if __name__ == '__main__':
+    main()
