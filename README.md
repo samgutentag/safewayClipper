fbb520976d98009449b01fa9d8dee7d61206d833
Sam Gutentag <github@samgutentag.com>
2018-12-06 00:03:30 -0800

    code docstring updates and additions, moved pieces into cleaner functions, should only look for new offers to clip, not all possible offers.  readme update formatting and webdriver download links

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-Safeway Coupon Clipper
+# Safeway Coupon Clipper
 
-A Simple tool to learn Selenium and Clip Grocery Coupons!
+## A Simple tool to learn Selenium and Clip Grocery Coupons!
+
+You will need to install ChromeDriver, download link here: [ChromeDriver Download](https://sites.google.com/a/chromium.org/chromedriver/)
+
+Most recently tested version is 2.44
+
+Be sure to download Operating System Specific Version!
--- safewayClipper.py
+++ safewayClipper.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-''' A Tool for Clipping Coupons on Safeway.com!
+
+''' safewayClipper.py:  A Tool for Clipping Coupons on Safeway.com
 
     You will need to install ChromeDriver, download link here:
         https://sites.google.com/a/chromium.org/chromedriver/
-    Current Version is 2.43
+    Current Version is 2.44
 
-    Download Operating System SPecific Version!
+    Download Operating System Specific Version!
 
 '''
 
@@ -19,16 +20,22 @@ __author__ = "Sam Gutentag"
 __copyright__ = "Copyright 2018, Sam Gutentag"
 __credits__ = ["Sam Gutentag"]
 __license__ = "GPL"
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 __maintainer__ = "Sam Gutentag"
 __email__ = "developer@samgutentag.com"
-__status__ = 'Production'
+__status__ = 'Developement'
+    # 'Production', 'Developement', 'Prototype'
 
+def click_offers_on_page(driver=None, page=None, button_title=None, scroll_limit=30):
 
-def click_offers_on_page(driver=None, page=None, button_class=None, scroll_limit=5):
+    ''' Scrolling and clicking elements with specified class tags
 
-    '''
-        Scrolling and clicking elements with specified class tags
+    Keyword arguments:
+        driver (webdriver)      --  the target Selenium web driver
+        page (string)           --  the webpage URL to search for offers
+        button_title (string)   --  the css class of the button to 'click'
+        scroll_limit (int)      --  page scrolls <n> times to load content
+                                    before looking for buttons to 'click'
     '''
 
     print('Loading page: %s' % page)
@@ -36,20 +43,20 @@ def click_offers_on_page(driver=None, page=None, button_class=None, scroll_limit
     # load page url
     driver.get(page)
 
-    time.sleep(10)
+    time.sleep(5)
 
-    # scroll to bottom of page
-    scroll_count = 0
-    while scroll_count < scroll_limit:
-        print('%d of %d: Scrolling...' % (scroll_count+1, scroll_limit))
+    for i in range(0, scroll_limit+1):
+        if i % 10 == 0 or i == 0 or i == scroll_limit:
+            print(f'{i} of {scroll_limit}: Scrolling...')
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(3)
-        scroll_count += 1
+        time.sleep(1)
 
-    time.sleep(15)
+    time.sleep(5)
 
-    print('Getting buttons...')
-    add_buttons = driver.find_elements_by_class_name(button_class)
+    print(f'Getting buttons...')
+    add_buttons = driver.find_elements_by_xpath(f'//*[@title="{button_title}"]')
+
+    print(f'Found {len(add_buttons)} total buttons')
 
     for idx, button in enumerate(add_buttons):
         print('%d of %d:\t%s' % (idx, len(add_buttons), button))
@@ -57,28 +64,27 @@ def click_offers_on_page(driver=None, page=None, button_class=None, scroll_limit
             button.click()
         except:
             pass
-        time.sleep(1)
 
-
-def main():
-
+def get_webdriver():
+    ''' setup webdriver, else set to None
+    '''
     try:
         chromedriver = './chromedriver'
         driver = webdriver.Chrome(chromedriver)
     except:
         print('Could not find chromedriver, using Firefox instead')
-        driver = webdriver.Firefox()
-    driver.get("https://www.safeway.com/CMS/account/login/")
+        driver = None
+    return driver
 
-    time.sleep(15)
+def login(driver=get_webdriver(), login_username = None, login_password = None):
 
+    ''' Login to Safeway member portal, defaults to env variable credientials
+
+    Keyword arguments:
+        driver (webdriver)      --  the target Selenium web driver
+        login_username (string) --  username for login, might be an email
+        login_password (string) --  password for login
     '''
-    #===========================================================================
-    #       Input Your Details Here!
-    #===========================================================================
-    '''
-    login_username = None
-    login_password = None
 
     # login data
     if not login_username:
@@ -103,33 +109,43 @@ def main():
         # click sign in button
         login_attempt = driver.find_element_by_id("create-account-btn")
         login_attempt.click()
-        print('login success!')
+        print('\tlogin success!')
+        return 0
+
     except Exception as e:
-        print('Whoops... Something went wrong.\n\t%s' % e)
+        print('Whoops... Something went wrong trying to login.\n\t%s' % e)
         return -1
 
-    time.sleep(15)
+def main():
+
+    driver = get_webdriver()
+    if not driver:
+        print('ERROR: Could not assign Webdriver...')
+        return -1
+
+    driver.get("https://www.safeway.com/CMS/account/login/")
+
+    time.sleep(5)
+
+    '''
+    #===========================================================================
+    #       Input Your Details Here!
+    #===========================================================================
+    '''
+    login(driver=driver)
+    time.sleep(10)
 
     # Just For U
     just_for_U_offers = 'https://www.safeway.com/ShopStores/Justforu-Coupons.page#/category/all'
-    add_button_class = 'lt-place-add-button'
+    button_title = 'Add'
 
-    click_offers_on_page(driver=driver, page=just_for_U_offers, button_class=add_button_class, scroll_limit=50)
-
-    time.sleep(15)
-
-    # Club Specials
-    club_special_offers = 'https://www.safeway.com/ShopStores/Justforu-YourClubSpecials.page?reloaded=true'
-    add_button_class = 'lt-add-offer'
-    click_offers_on_page(driver=driver, page=club_special_offers, button_class=add_button_class, scroll_limit=10)
+    click_offers_on_page(driver=driver, page=just_for_U_offers, button_title=button_title)
 
     driver.quit()
     print('All Done! Happy Shopping!')
 
-
 if __name__ == '__main__':
     main()
-
 
 
 # EOF
