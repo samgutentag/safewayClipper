ce0dc3bac3cf490dc2c81912307fa5fba2373066
Sam Gutentag <github@samgutentag.com>
2018-12-08 10:01:30 -0800

    better collection of offer buttons, clicks limited to non previously added offers for efficiency

--- safewayClipper.py
+++ safewayClipper.py
@@ -20,34 +20,35 @@ __author__ = "Sam Gutentag"
 __copyright__ = "Copyright 2018, Sam Gutentag"
 __credits__ = ["Sam Gutentag"]
 __license__ = "GPL"
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 __maintainer__ = "Sam Gutentag"
 __email__ = "developer@samgutentag.com"
 __status__ = 'Developement'
     # 'Production', 'Developement', 'Prototype'
 
-def click_offers_on_page(driver=None, page=None, button_title=None, scroll_limit=30):
+def click_offers_on_page(driver=None, page=None, button_title=None,
+                                        button_class=None, scroll_limit=30):
 
     ''' Scrolling and clicking elements with specified class tags
 
     Keyword arguments:
         driver (webdriver)      --  the target Selenium web driver
         page (string)           --  the webpage URL to search for offers
-        button_title (string)   --  the css class of the button to 'click'
+        button_title (string)   --  the text label of the button to 'click'
+        button_class (string)   --  the css class of the button to 'click'
         scroll_limit (int)      --  page scrolls <n> times to load content
                                     before looking for buttons to 'click'
     '''
 
-    print('Loading page: %s' % page)
+    print(f'Loading page: {page}')
 
     # load page url
     driver.get(page)
 
     time.sleep(5)
 
+    print(f'Scrolling...')
     for i in range(0, scroll_limit+1):
-        # if i % 10 == 0 or i == 0 or i == scroll_limit:
-        #     print(f'{i} of {scroll_limit}: Scrolling...')
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(1)
 
@@ -56,14 +57,28 @@ def click_offers_on_page(driver=None, page=None, button_title=None, scroll_limit
     print(f'Getting buttons...')
     add_buttons = driver.find_elements_by_xpath(f'//*[@title="{button_title}"]')
 
-    print(f'Found {len(add_buttons)} total buttons')
+    print(f'Found {len(add_buttons)} buttons by searching text...')
 
-    for idx, button in enumerate(add_buttons):
-        print('%d of %d:\t%s' % (idx, len(add_buttons), button))
+    if len(add_buttons) < 1 and not button_class is None:
+        add_buttons = driver.find_elements_by_class_name(button_class)
+
+    print(f'Found {len(add_buttons)} total buttons by searching css class...')
+
+    valid_buttons = [x for x in add_buttons if x.text == 'Add']
+    print(f'Found {len(valid_buttons)} total buttons to click!')
+
+    invalid_buttons = [x for x in add_buttons if x.text == 'Added']
+    print(f'Found {len(invalid_buttons)} total buttons to ignore!')
+
+    for idx, button in enumerate(valid_buttons):
+        # print('%d of %d:\t%s' % (idx, len(valid_buttons), button))
+        print(f'{idx} of {len(valid_buttons)}:\t{button}')
         try:
             button.click()
         except:
             pass
+
+    return driver
 
 def get_webdriver():
     ''' setup webdriver, else set to None
@@ -72,7 +87,7 @@ def get_webdriver():
         chromedriver = './chromedriver'
         driver = webdriver.Chrome(chromedriver)
     except:
-        print('Could not find chromedriver, using Firefox instead')
+        print(f'Could not find chromedriver')
         driver = None
     return driver
 
@@ -92,7 +107,7 @@ def login(driver=get_webdriver(), login_username = None, login_password = None):
     if not login_password:
         login_password = os.environ.get('SAFEWAY_PASSWORD')
 
-    print('Retrieved login credentials')
+    print(f'Retrieved login credentials')
 
     # refresh page - Safeway's login page doesnt load properly the first time
     print('refreshing page...')
@@ -100,7 +115,7 @@ def login(driver=get_webdriver(), login_username = None, login_password = None):
     time.sleep(5)
 
 
-    print('attempting login...')
+    print(f'attempting login...')
     try:
         username = driver.find_element_by_id("input-email")
         password = driver.find_element_by_id("password-password")
@@ -109,18 +124,18 @@ def login(driver=get_webdriver(), login_username = None, login_password = None):
         # click sign in button
         login_attempt = driver.find_element_by_id("create-account-btn")
         login_attempt.click()
-        print('\tlogin success!')
+        print(f'\tlogin success!')
         return 0
 
     except Exception as e:
-        print('Whoops... Something went wrong trying to login.\n\t%s' % e)
+        print(f'Whoops... Something went wrong trying to login.\n\t{e}')
         return -1
 
 def main():
 
     driver = get_webdriver()
     if not driver:
-        print('ERROR: Could not assign Webdriver...')
+        print(f'ERROR: Could not assign Webdriver...')
         return -1
 
     driver.get("https://www.safeway.com/CMS/account/login/")
@@ -138,9 +153,12 @@ def main():
     # Just For U
     just_for_U_offers = 'https://www.safeway.com/ShopStores/Justforu-Coupons.page#/category/all'
     button_title = 'Add'
+    button_class = 'lt-place-add-button'
 
-    click_offers_on_page(driver=driver, page=just_for_U_offers, button_title=button_title)
+    driver = click_offers_on_page(driver=driver, page=just_for_U_offers,
+                            button_title=button_title, button_class=button_class)
 
+    driver.close()
     driver.quit()
     print('All Done! Happy Shopping!')
 
