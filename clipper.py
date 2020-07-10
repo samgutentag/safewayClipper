b1f175212218f2256de6c844704b108510074126
Sam Gutentag <developer@samgutentag.com>
2020-07-10 09:09:45 -0700

    header updates

--- clipper.py
+++ clipper.py
@@ -17,29 +17,15 @@ of coupons found.  Keep in mind that this number of coupons found may be
 truncated from the total number of available coupons by the scroll limit.
 
 
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation, either version 3 of the License, or (at your option) any later
-version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with
-this program. If not, see <http://www.gnu.org/licenses/>.
+Use CalVer versioning from here https://calver.org/
 
 """
 
-
 __authors__ = ["Sam Gutentag"]
 __email__ = "developer@samgutentag.com"
-__date__ = "2020/02/28"
-__deprecated__ = False
-__license__ = "GPLv3"
 __maintainer__ = "Sam Gutentag"
-__status__ = "Developement"
-__version__ = "0.8.1"
-# "Prototype", "Development", "Production", or "Legacy"
+__version__ = "2020.04.29dev"
+# "dev", "alpha", "beta", "rc1"
 
 
 import argparse
